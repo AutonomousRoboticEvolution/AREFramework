1b8d6334abf482ace94336c69e92cc775bf9a306
bakpanfrankman <frank_arena50@hotmail.com>
2019-01-21 11:32:28 +0000

    Linux support for Ecosystem manager

--- WP4_Ecosytem_Manager/Cplusplus_Evolution/ERFiles/EcosystemManager/EcosystemManager.py
+++ WP4_Ecosytem_Manager/Cplusplus_Evolution/ERFiles/EcosystemManager/EcosystemManager.py
@@ -2,38 +2,49 @@ from tkinter import *
 import subprocess
 import sys, string, os
 
+directory = os.getcwd()
+if (os.name == 'nt'):
+	print("Running windows")
+	directory = "C:/Program Files/V-REP3/V-REP_PRO_EDU/"
+else:
+	print("Not running windows")
+
+print("directory = " + directory)
+
 window = Tk()
 window.title("ARE Ecosystem Manager")
 lbl = Label(window,text="Minimal Ecosystem Manager")
 lbl.grid(column=0, row =0)
 con = Label(window,text="Output")
 con.grid(column=0, row =6)
-repLab = Label(window,text="C:/Program Files/V-REP3/V-REP_PRO_EDU/")
+repLab = Label(window,text=directory)
 repLab.grid(column=0, row = 5)
 window.geometry('800x600')
 repository=Entry(window,width=100)
 repository.grid(column=0,row=4)
-repository.configure(text="C:/Program Files/V-REP3/V-REP_PRO_EDU/")
+repository.configure(text=directory)
 
 def clickRun():
 	con.configure(text="Starting Evolution")
-	repository.configure(text="C:/Program Files/V-REP3/V-REP_PRO_EDU/")
-	subprocess.Popen([r"C:/Program Files/V-REP3/V-REP_PRO_EDU/vrep.exe", "-g0", "-g2", "-gfiles"])
+	repository.configure(text=directory)
+	subprocess.Popen([r""+ directory + "vrep.exe", "-g0", "-g2", "-gfiles"])
 	# file = 'C:\\Program Files\\V-REP3\\V-REP_PRO_EDU\\vrep.exe'
 	# os.system('"'+ file + '"' + ' -g10 - g2 -gfiles')
 
 def clickRecallBest():
 	con.configure(text="RECALLING BEST")
-	repository.configure(text="C:/Program Files/V-REP3/V-REP_PRO_EDU/")
-	subprocess.Popen([r"C:/Program Files/V-REP3/V-REP_PRO_EDU/vrep.exe", "-g0", "-g9", "-gfiles"])
+	repository.configure(text=directory)
+	subprocess.Popen([r""+ directory + "vrep.exe", "-g0", "-g9", "-gfiles"])
 	
 def clickRunParallel():
 	con.configure(text="Starting Parallel Evolution")
-	repository.configure(text="C:/Program Files/V-REP3/V-REP_PRO_EDU/")
+	repository.configure(text=directory)
 	for i in range(7):
 		#arguments = "-h -g" + str(i) + " -g2 -gfiles -gREMOTEAPISERVERSERVICE_"+ str(i + 1) +"_FALSE_FALSE"
-		subprocess.Popen([r"C:/Program Files/V-REP3/V-REP_PRO_EDU/vrep.exe" ,"-h","-g0" ,"-g1" ,"-gfiles", "-gREMOTEAPISERVERSERVICE_"+str(i+104000)+"_TRUE_TRUE"])
+		subprocess.Popen([r""+directory+"vrep.exe" ,"-h","-g0" ,"-g1" ,"-gfiles", "-gREMOTEAPISERVERSERVICE_"+str(i+104000)+"_TRUE_TRUE"])
 				   #-gREMOTEAPISERVERSERVICE_"+ str(i+1040000) + "_FALSE_FALSE"])
+
+
 
 runB = Button(window, text="Run Evolution", command=clickRun)
 recallB = Button(window, text="Recall Best", command=clickRecallBest)
