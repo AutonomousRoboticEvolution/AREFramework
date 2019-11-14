7950ec80e8ac7eaa51ffff419bdedb9b3da6f95f
Léni K. Le Goff <lenilegoff@gmail.com>
2019-11-14 16:22:23 +0000

    move CPPNGenome and NNControl in hyperneat experiment

--- EvolutionVREP/src/ARE/CPPNGenome.cpp
+++
@@ -1,9 +0,0 @@
-#include "ARE/CPPNGenome.h"
-
-using namespace are;
-
-Phenotype::Ptr CPPNGenome::develop(){
-    CPPN cppn;
-    neat_genome.BuildPhenotype(cppn.nn);
-    return std::make_shared<CPPN>(cppn);
-}
