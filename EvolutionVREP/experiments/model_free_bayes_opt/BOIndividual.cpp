0f52593d5e020ecc194e665b542f1f827eadfb1e
Leni K. Le Goff <l.legoff2@napier.ac.uk>
2019-12-17 17:39:25 +0000

    add new experiment Model Free BO only

---
+++ EvolutionVREP/experiments/model_free_bayes_opt/BOIndividual.cpp
@@ -0,0 +1,70 @@
+#include "BOIndividual.h"
+
+using namespace are;
+
+BOIndividual::BOIndividual(const Genome::Ptr& morph_gen,const Genome::Ptr& ctrl_gen) :
+    Individual(morph_gen,ctrl_gen)
+{
+
+}
+
+BOIndividual::BOIndividual(const Genome::Ptr& morph_gen,const Genome::Ptr& ctrl_gen, const BOLearner::Ptr& l) :
+    Individual(morph_gen,ctrl_gen)
+{
+    learner = l;
+}
+
+Individual::Ptr BOIndividual::clone()
+{
+    return std::make_shared<BOIndividual>(*this);
+}
+
+void BOIndividual::update(double delta_time)
+{
+    int instance_type = settings::getParameter<settings::Integer>(parameters,"#instanceType").value;
+
+    std::vector<double> inputs = morphology->update();
+    std::vector<double> outputs = control->update(inputs);
+
+    std::vector<int> jointHandles =
+            std::dynamic_pointer_cast<EPuckMorphology>(morphology)->get_jointHandles();
+
+    assert(jointHandles.size() == outputs.size());
+
+    for (size_t i = 0; i < outputs.size(); i++){
+        sim::setJointVelocity(instance_type,
+                              jointHandles[i],static_cast<float>(outputs[i]),properties->clientID);
+    }
+}
+
+void BOIndividual::createMorphology()
+{
+    morphology.reset(new EPuckMorphology(parameters));
+    morphology->set_properties(properties);
+    morphology->createAtPosition(0,0,0);
+}
+
+void BOIndividual::createController()
+{
+    control.reset(new NNControl);
+    control->set_properties(properties); 
+    NEAT::NeuralNetwork nn;
+    std::dynamic_pointer_cast<NEATGenome>(ctrlGenome)->neat_genome.BuildPhenotype(nn);
+    std::dynamic_pointer_cast<NNControl>(control)->nn = nn;
+}
+
+
+void BOIndividual::update_learner(std::vector<Eigen::VectorXd> &obs, std::vector<Eigen::VectorXd> &spl)
+{
+    std::dynamic_pointer_cast<BOLearner>(learner)->set_observation(obs);
+    std::dynamic_pointer_cast<BOLearner>(learner)->set_samples(spl);
+    learner->update(control);
+}
+
+void BOIndividual::update_learner_model(std::vector<Eigen::VectorXd> &obs, std::vector<Eigen::VectorXd> &spl)
+{
+    std::dynamic_pointer_cast<BOLearner>(learner)->set_observation(obs);
+    std::dynamic_pointer_cast<BOLearner>(learner)->set_samples(spl);
+    std::dynamic_pointer_cast<BOLearner>(learner)->update_model();
+}
+
---
+++ EvolutionVREP/experiments/model_free_bayes_opt/BOIndividual.h
@@ -0,0 +1,36 @@
+#ifndef BOINDIVIDUAL_H
+#define BOINDIVIDUAL_H
+
+#include "ARE/Individual.h"
+#include "EPuckMorphology.h"
+#include "BOLearner.h"
+#include "v_repLib.h"
+
+#include "NEATGenome.h"
+
+namespace are {
+
+class BOIndividual : public Individual
+{
+public :
+    BOIndividual() : Individual(){}
+    BOIndividual(const Genome::Ptr& morph_gen,const Genome::Ptr& ctrl_gen);
+    BOIndividual(const Genome::Ptr& morph_gen,const Genome::Ptr& ctrl_gen, const BOLearner::Ptr& l);
+
+    Individual::Ptr clone();
+
+    void update(double delta_time);
+
+    void update_learner(std::vector<Eigen::VectorXd> &obs, std::vector<Eigen::VectorXd> &spl);
+    void update_learner_model(std::vector<Eigen::VectorXd> &obs, std::vector<Eigen::VectorXd> &spl);
+    const Learner::Ptr &getLearner(){return learner;}
+
+
+protected:
+    void createController() override;
+    void createMorphology() override;
+};
+
+}//are
+
+#endif //BOINDIVIDUAL_H
