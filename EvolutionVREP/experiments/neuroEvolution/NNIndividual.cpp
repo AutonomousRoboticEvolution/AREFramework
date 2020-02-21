7c0e5b87fdc479eb8493a2196d8e7826cdf5134d
Léni K. Le Goff <lenilegoff@gmail.com>
2020-02-21 10:37:58 +0000

    add new elements for neuroevolution

---
+++ EvolutionVREP/experiments/neuroEvolution/NNIndividual.cpp
@@ -0,0 +1,60 @@
+#include "NNIndividual.hpp"
+
+using namespace are;
+
+void NNIndividual::createController(){
+    NEAT::Genome gen =
+            std::dynamic_pointer_cast<NNGenome>(ctrlGenome)->get_nn_genome();
+    NEAT::Substrate subs = morphology->get_substrate();
+    control.reset(new NNControl);
+    control->set_parameters(parameters);
+    NEAT::NeuralNetwork nn;
+    gen.BuildPhenotype(nn);
+    std::dynamic_pointer_cast<NNControl>(control)->nn = nn;
+}
+
+void NNIndividual::createMorphology(){
+    morphology.reset(new EPuckMorphology(parameters));
+    std::dynamic_pointer_cast<EPuckMorphology>(morphology)->loadModel();
+
+    float init_x = settings::getParameter<settings::Float>(parameters,"#init_x").value;
+    float init_y = settings::getParameter<settings::Float>(parameters,"#init_y").value;
+    float init_z = settings::getParameter<settings::Float>(parameters,"#init_z").value;
+
+    morphology->createAtPosition(init_x,init_y,init_z);
+}
+
+void NNIndividual::update(double delta_time){
+    std::vector<double> inputs = morphology->update();
+
+    std::vector<double> outputs = control->update(inputs);
+
+
+    std::vector<int> jointHandles =
+            std::dynamic_pointer_cast<EPuckMorphology>(morphology)->get_jointHandles();
+
+    assert(jointHandles.size() == outputs.size());
+
+    for (size_t i = 0; i < outputs.size(); i++){
+        simSetJointTargetVelocity(jointHandles[i],static_cast<float>(outputs[i]));
+    }
+}
+
+std::string NNIndividual::to_string()
+{
+    std::stringstream sstream;
+    boost::archive::text_oarchive oarch(sstream);
+    oarch.register_type<NNIndividual>();
+    oarch.register_type<NNGenome>();
+    oarch << *this;
+    return sstream.str();
+}
+
+void NNIndividual::from_string(const std::string &str){
+    std::stringstream sstream;
+    sstream << str;
+    boost::archive::text_iarchive iarch(sstream);
+    iarch.register_type<NNIndividual>();
+    iarch.register_type<NNGenome>();
+    iarch >> *this;
+}
