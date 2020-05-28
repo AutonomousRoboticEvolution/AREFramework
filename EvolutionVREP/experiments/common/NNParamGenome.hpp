e86c7d889f12d681ce20b8d71345bacc0423174d
MoxAntia <lenilegoff@gmail.com>
2020-05-28 15:25:40 +0100

    add to NNParamGenome mutation operator

--- EvolutionVREP/experiments/common/NNParamGenome.hpp
+++ EvolutionVREP/experiments/common/NNParamGenome.hpp
@@ -2,8 +2,73 @@
 #define NN_PARAM_GENOME_HPP
 
 #include <ARE/Genome.h>
+#include <boost/random.hpp>
 
 namespace are{
+
+using mutators_t = std::function<void(std::vector<double>&, double* param, const misc::RandNum::Ptr&)>;
+
+struct mutators{
+
+    enum type{
+        UNIFORM = 0,
+        GAUSSIAN = 1,
+        POLYNOMIAL = 2
+    };
+
+    /**
+     * @brief uniform mutation operator
+     *  Parameters :
+     *      0 mutation rate
+     *      1 upper bound
+     *      2 lower bound
+     */
+    static mutators_t uniform;
+
+    /**
+     * @brief gaussian mutation operator.
+     *  Parameters :
+     *      0 mutation rate
+     *      1 sigma
+     *      2 upper bound
+     *      3 lower bound
+     */
+    static mutators_t gaussian;
+
+    /**
+     * @brief polynomial mutation operator
+     *  Parameters :
+     *      0 mutation rate
+     *      1 eta
+     *      2 upper bound
+     *      3 lower bound
+     */
+    static mutators_t polynomial;
+};
+
+
+
+
+using crossover_t = std::function<void(const std::vector<double> &p1, const std::vector<double> &p2,
+                                   std::vector<double> &c1,std::vector<double>& c2)>;
+
+struct crossovers{
+    enum type {
+        NO_CROSSOVER = 0,
+        RECOMBINATION = 1,
+        SBX = 2
+    };
+
+    /**
+     * @brief recombination
+     */
+    static crossover_t recombination;
+
+    /**
+     * @brief simulated binary crossover
+     */
+    static crossover_t sbx;
+};
 
 class NNParamGenome : public Genome
 {
@@ -24,9 +89,11 @@ public:
     }
 
 
-    void init() override{}
+    void init() override{
+
+    }
     Phenotype::Ptr develop() override{}
-    void mutate() override{}
+    void mutate() override;
 
 
     void set_weights(const std::vector<double>& w){weights = w;}
