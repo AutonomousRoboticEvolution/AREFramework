a0d32bc2911876fa6dc1b466cda0efbabb5bebaf
MoxAntia <lenilegoff@gmail.com>
2020-05-29 12:14:56 +0100

    remove nn_type (moved in core framework)

--- EvolutionVREP/experiments/common/settings.hpp
+++ EvolutionVREP/experiments/common/settings.hpp
@@ -5,13 +5,6 @@ namespace are {
 
 namespace settings {
 
-typedef enum nn_type{
-    FFNN = 0,
-    CTRNN = 1,
-    ELMAN = 2,
-    CPG = 3
-} nn_type;
-
 enum genomeType {
     NEAT = 0,
     NN = 1,
