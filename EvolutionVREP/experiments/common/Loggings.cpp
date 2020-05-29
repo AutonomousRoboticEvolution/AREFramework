bee54923bb63936a65852b72902eef53d44302e3
MoxAntia <lenilegoff@gmail.com>
2020-05-29 12:14:04 +0100

    replace NNGenomeLog by NNParamGenomeLog

--- EvolutionVREP/experiments/common/Loggings.cpp
+++ EvolutionVREP/experiments/common/Loggings.cpp
@@ -34,13 +34,19 @@ void EvalTimeLog::saveLog(EA::Ptr &ea){
     logFileStream.close();
 }
 
-void NNGenomeLog::saveLog(EA::Ptr &ea)
+void NNParamGenomeLog::saveLog(EA::Ptr &ea)
 {
     int generation = ea->get_generation();
+
+    std::ofstream logFileStream;
     for(size_t i = 0; i < ea->get_population().size(); i++){
-        NEAT::Genome genome = std::dynamic_pointer_cast<NNGenome>(ea->getIndividual(i)->get_ctrl_genome())->get_nn_genome();
-        std::stringstream filepath;
-        filepath << Logging::log_folder << "/genome_" << generation << "_" << i;
-        genome.Save(filepath.str().c_str());
+        std::stringstream filename;
+        filename << "genome_" << generation << "_" << i;
+        if(!openOLogFile(logFileStream, filename.str()))
+            return;
+        logFileStream << std::dynamic_pointer_cast<NNParamGenome>(
+                             ea->get_population()[i]->get_ctrl_genome()
+                             )->to_string();
+        logFileStream.close();
     }
 }
