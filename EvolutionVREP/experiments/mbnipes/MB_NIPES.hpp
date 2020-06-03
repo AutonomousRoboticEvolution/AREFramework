aa7b03e71bcdaf4f759f0e3ffbbb58f9d876dff5
MoxAntia <lenilegoff@gmail.com>
2020-06-03 11:52:00 +0100

    add new experiment : Model Based NIP-ES

---
+++ EvolutionVREP/experiments/mbnipes/MB_NIPES.hpp
@@ -0,0 +1,51 @@
+#ifndef CMABO_HPP
+#define CMABO_HPP
+
+#include "NIPES.hpp"
+#include "NN2Individual.hpp"
+#include "BOLearner.h"
+
+namespace are
+{
+
+using obs_fct_t = std::function<Eigen::VectorXd(const NN2Individual::Ptr &)>;
+
+struct obs_fcts{
+    /**
+     * @brief Observation corresponding to the final position of the robot
+     */
+    static obs_fct_t final_position;
+    static obs_fct_t reward;
+
+};
+
+class MB_NIPES : public NIPES
+{
+
+public:
+
+    typedef std::unique_ptr<MB_NIPES> Ptr;
+    typedef std::unique_ptr<const MB_NIPES> ConstPtr;
+
+    MB_NIPES() : NIPES(){}
+    MB_NIPES(const settings::ParametersMapPtr& param) : NIPES(param){}
+
+    void init();
+    void epoch();
+    void init_next_pop();
+    bool update(const Environment::Ptr&);
+    Eigen::VectorXd getLastObs() const {return observations.back();}
+    Eigen::VectorXd getLastSpl() const {return samples.back();}
+    const BOLearner::Ptr &get_learner() const {return learner;}
+protected:
+    obs_fct_t _compute_obs;
+    std::vector<Eigen::VectorXd> observations;
+    std::vector<Eigen::VectorXd> samples;
+    BOLearner::Ptr learner;
+    std::vector<Individual::Ptr> virtual_pop;
+};
+
+}
+
+
+#endif //CMABO_HPP
