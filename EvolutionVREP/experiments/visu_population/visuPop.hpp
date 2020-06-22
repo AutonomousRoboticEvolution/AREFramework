8b5e31a0bd82e4f2edf190c878b48d9f692b2b51
MoxAntia <lenilegoff@gmail.com>
2020-06-22 11:53:24 +0100

    simplification of visu_pop and adaptation to NN2Individual

--- EvolutionVREP/experiments/visu_population/visuPop.hpp
+++ EvolutionVREP/experiments/visu_population/visuPop.hpp
@@ -9,6 +9,7 @@
 #include "NNParamGenome.hpp"
 #include "NNGenome.hpp"
 #include "NNControl.h"
+#include "NN2Individual.hpp"
 #include "FixedMorphology.hpp"
 #include "settings.hpp"
 
@@ -16,33 +17,33 @@
 namespace are {
 
 
-class VisuIndividual : public Individual
-{
-public:
-    VisuIndividual() : Individual(){}
-    VisuIndividual(const Genome::Ptr& morph_gen,const Genome::Ptr& ctrl_gen) :
-        Individual(morph_gen,ctrl_gen){}
-    VisuIndividual(const VisuIndividual& ind) :
-        Individual(ind),
-        final_position(ind.final_position){}
+//class VisuIndividual : public Individual
+//{
+//public:
+//    VisuIndividual() : Individual(){}
+//    VisuIndividual(const Genome::Ptr& morph_gen,const Genome::Ptr& ctrl_gen) :
+//        Individual(morph_gen,ctrl_gen){}
+//    VisuIndividual(const VisuIndividual& ind) :
+//        Individual(ind),
+//        final_position(ind.final_position){}
 
-    Individual::Ptr clone() override {
-        return std::make_shared<VisuIndividual>(*this);
-    }
+//    Individual::Ptr clone() override {
+//        return std::make_shared<VisuIndividual>(*this);
+//    }
 
-    void update(double delta_time) override;
+//    void update(double delta_time) override;
 
-    void set_final_position(const std::vector<double> fp){final_position = fp;}
-    const std::vector<double> get_final_position(){return final_position;}
+//    void set_final_position(const std::vector<double> fp){final_position = fp;}
+//    const std::vector<double> get_final_position(){return final_position;}
 
 
 
-protected:
-    void createMorphology() override;
-    void createController() override;
+//protected:
+//    void createMorphology() override;
+//    void createController() override;
 
-    std::vector<double> final_position;
-};
+//    std::vector<double> final_position;
+//};
 
 class VisuPop : public EA
 {
