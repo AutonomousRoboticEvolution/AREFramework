921d08e7da5f0358741bbe78a76bff4538bbfd43
MoxAntia <lenilegoff@gmail.com>
2020-06-23 17:28:20 +0100

    remove behav log from cmabo

--- EvolutionVREP/experiments/cmabo/CMABOLoggings.cpp
+++ EvolutionVREP/experiments/cmabo/CMABOLoggings.cpp
@@ -2,26 +2,6 @@
 
 using namespace are;
 
-void BehavDescLog::saveLog(EA::Ptr &ea)
-{
-    std::ofstream logFileStream;
-    if(!openOLogFile(logFileStream))
-        return;
-
-    int generation = ea->get_generation();
-
-    logFileStream << "generation," << generation << ",";
-    for(size_t i = 0; i < ea->get_population().size(); i++){
-        logFileStream << i << ",";
-        for(const double &pos :
-            std::dynamic_pointer_cast<NN2Individual>(ea->getIndividual(i))->get_final_position())
-            logFileStream << pos << ",";
-    }
-    logFileStream << std::endl;
-
-    logFileStream.close();
-}
-
 void StopCritLog::saveLog(EA::Ptr &ea)
 {
     if(!static_cast<NIPES*>(ea.get())->restarted())
