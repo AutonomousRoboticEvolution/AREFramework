bc41c4ad8923d8b3cb6929d0750b1e5325e341be
MoxAntia <lenilegoff@gmail.com>
2020-11-06 14:11:30 +0000

    add VirtualEnvironment abstract class. Environment becomes more abstract. Add image processing algorithm to track unicolor blob from a topdown camera

---
+++ EvolutionVREP/include/ARE/realMaze.hpp
@@ -0,0 +1,27 @@
+#ifndef REALMAZE_HPP
+#define REALMAZE_HPP
+
+#include "ARE/Environment.h"
+#include "image_processing/blob_tracking.hpp"
+
+
+namespace are {
+
+class RealMaze : public Environment
+{
+public:
+    void init() override;
+    std::vector<double> fitnessFunction(const Individual::Ptr &ind) override;
+    void update_info() override;
+
+private:
+    std::vector<double> current_position;
+    std::vector<double> target_position;
+
+    cv::VideoCapture video_capture;
+    std::pair<cv::Scalar,cv::Scalar> colour_range;
+};
+
+} //are
+
+#endif //REALMAZE_HPP
