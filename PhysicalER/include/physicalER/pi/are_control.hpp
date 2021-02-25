7a270b70a373e06c5923b209a701f9a40a4038fe
Matt <matt.hale@brl.ac.uk>
2021-02-25 10:34:57 +0000

    are_control now interfaces with wheels to run controller

--- PhysicalER/include/physicalER/pi/are_control.hpp
+++ PhysicalER/include/physicalER/pi/are_control.hpp
@@ -1,10 +1,14 @@
 #ifndef ARE_CONTROL_HPP
 #define ARE_CONTROL_HPP
 
+#define NEURAL_NETWORK_OUTPUT_TO_WHEEL_INPUT_MULTIPLIER 30.f
+
 #include <iostream>
 #include <vector>
 #include "physicalER/pi/pi_individual.hpp"
 #include "MotorOrgan.hpp"
+#include "DaughterBoards.hpp"
+#include "LedDriver.hpp"
 #include <zmq.hpp>
 
 namespace are {
@@ -22,10 +26,14 @@ public:
 
 private:
     NN2Individual controller;
-    float _max_eval_time = 10; // seconds
-    float _time_step = 0.1; // seconds
-    
+    float _max_eval_time = 3000000; // microseconds
+    float _time_step = 100000; // microseconds
+
     std::shared_ptr<MotorOrgan> wheel0;
+    std::shared_ptr<MotorOrgan> wheel1;
+    std::shared_ptr<MotorOrgan> wheel2;
+    std::shared_ptr<DaughterBoards> daughterBoards;
+    std::shared_ptr<LedDriver> ledDriver;
 };
 
 }//pi
--- PhysicalER/src/pi/are_control.cpp
+++ PhysicalER/src/pi/are_control.cpp
@@ -4,12 +4,39 @@ using namespace are::pi;
 
 AREControl::AREControl(const NN2Individual &ind){
     controller = ind;
+
+	// need to turn on the daughter boards
+	daughterBoards->init();
+    daughterBoards->turnOn();
+
     // each organ needs to be initiated with its i2c address.
     wheel0.reset(new MotorOrgan(0x60));
+    wheel1.reset(new MotorOrgan(0x61));
+    wheel2.reset(new MotorOrgan(0x62));
+
+    // initialise the LED and flash green to show ready
+    ledDriver.reset(new LedDriver(0x6A)); // <- the Led driver is always the same i2c address, it cannot be cahnged
+    ledDriver->init();
+    ledId leds[4] = {RGB0, RGB1, RGB2, RGB3};
+    for (int i=0; i<4; ++i) { // loop through and turn on all the LEDS
+        ledDriver->setMode(leds[i], PWM, ALL);
+        ledDriver->setBrightness(leds[i], 150);
+        ledDriver->setColour(leds[i],GREEN);
+    }
+    usleep(500000); // 0.5 second
+    for (int i=0; i<4; ++i) { // loop through and turn off all the LEDS
+        ledDriver->setMode(leds[i],FULL_OFF,ALL);
+    }
 }
 
+// For each ouput from the controller, send the required value to the low-level wheel object
 void AREControl::sendMotorCommands(std::vector<double> values){
-    wheel0->setSpeed(values[0]*50.f);
+    std::cout << "wheel0 setSpeed to: " << values[0]*NEURAL_NETWORK_OUTPUT_TO_WHEEL_INPUT_MULTIPLIER << std::endl;
+    std::cout << "wheel1 setSpeed to: " << values[1]*NEURAL_NETWORK_OUTPUT_TO_WHEEL_INPUT_MULTIPLIER << std::endl;
+    std::cout << "wheel2 setSpeed to: " << values[2]*NEURAL_NETWORK_OUTPUT_TO_WHEEL_INPUT_MULTIPLIER << std::endl;
+    wheel0->setSpeed(values[0]*NEURAL_NETWORK_OUTPUT_TO_WHEEL_INPUT_MULTIPLIER);
+    wheel1->setSpeed(values[1]*NEURAL_NETWORK_OUTPUT_TO_WHEEL_INPUT_MULTIPLIER);
+    wheel2->setSpeed(values[2]*NEURAL_NETWORK_OUTPUT_TO_WHEEL_INPUT_MULTIPLIER);
 }
 
 void AREControl::retrieveSensorValues(std::vector<double> &sensor_vals){
@@ -32,6 +59,12 @@ int AREControl::exec(int argc, char** argv, zmq::socket_t& socket){
         controller.set_inputs(sensor_values);
         controller.update(time);
         nn_outputs = controller.get_ouputs();
+
+        // some debugging messages
+        std::cout << "Time now: " << time << std::endl;
+        std::cout << "nn_outputs 0: " << nn_outputs[0] << std::endl;
+        std::cout << "nn_outputs 1: " << nn_outputs[1] << std::endl;
+        std::cout << "nn_outputs 2: " << nn_outputs[2] << std::endl;
         
         // send the new values to the actuators
         sendMotorCommands(nn_outputs);
@@ -45,8 +78,15 @@ int AREControl::exec(int argc, char** argv, zmq::socket_t& socket){
         socket.send(message);
         
         // wait for next timestep
-        sleep(_time_step);
+        usleep(_time_step);
     }
+
+    // turn everything off
+    wheel0->standby();
+    wheel1->standby();
+    wheel2->standby();
+    daughterBoards->turnOff();
+
     
     // send finished message
     zmq::message_t message(40);
