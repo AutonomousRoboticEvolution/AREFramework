03c1112f921f7541667b8b98098c0b75207db8c9
ebb505@york.ac.uk <ebb505@york.ac.uk>
2021-03-25 12:05:10 +0000

    Body plan testing experiment added to branch

---
+++ EvolutionVREP/experiments/bodyplantesting/CPPNIndividual.h
@@ -0,0 +1,93 @@
+#ifndef CPPNINDIVIDUAL_H
+#define CPPNINDIVIDUAL_H
+
+#include "ARE/nn2/NN2Individual.hpp"
+#include "ARE/CPPNGenome.h"
+#include "ARE/Morphology_CPPNMatrix.h"
+#include "v_repLib.h"
+#include "eigen_boost_serialization.hpp"
+
+// Learning
+#include "ARE/nn2/NN2Control.hpp"
+#include "ARE/NNParamGenome.hpp"
+#include "ARE/nn2/NN2Settings.hpp"
+#include "ARE/Settings.h"
+#include "nn2/mlp.hpp"
+#include "nn2/elman.hpp"
+#include "nn2/rnn.hpp"
+
+namespace are {
+
+class CPPNIndividual : public NN2Individual
+{
+public :
+    CPPNIndividual() : NN2Individual(){}
+    CPPNIndividual(const Genome::Ptr& morph_gen,const NNParamGenome::Ptr& ctrl_gen) :
+            NN2Individual(morph_gen, ctrl_gen){}
+    CPPNIndividual(const CPPNIndividual& ind):
+            NN2Individual(ind),
+            nn(ind.nn),
+            testRes(ind.testRes),
+            morphDesc(ind.morphDesc),
+            nn_inputs(ind.nn_inputs),
+            nn_outputs(ind.nn_outputs)
+    {}
+    Individual::Ptr clone() override{
+        return std::make_shared<CPPNIndividual>(*this);
+    }
+
+    template<class archive>
+    void serialize(archive &arch, const unsigned int v)
+    {
+        arch & objectives;
+        arch & ctrlGenome;
+        arch & morphGenome;
+        arch & final_position;
+        arch & nn_inputs;
+        arch & nn_outputs;
+
+    }
+    // Serialization
+    std::string to_string();
+    void from_string(const std::string &str);
+
+    // Setters and getters
+    NEAT::NeuralNetwork getGenome(){return nn;};
+
+    std::vector<bool> getManRes(){return testRes;};
+    void setGenome();
+
+    void setManRes();
+
+    /// Setters for descritors
+    void setMorphDesc();
+    void setGraphMatrix();
+    void setSymDesc();
+    void set_nn_inputs(int nni){nn_inputs = nni;}
+    void set_nn_outputs(int nno){nn_outputs = nno;}
+    /// Getters for descritors
+    Eigen::VectorXd getMorphDesc(){return morphDesc;};
+
+    Eigen::VectorXd descriptor();
+
+
+    void update(double delta_time) override;
+
+protected:
+    void createMorphology() override;
+
+    NEAT::NeuralNetwork nn;
+    std::vector<bool> testRes;
+
+    /// Descritors
+    Eigen::VectorXd morphDesc;
+
+private:
+
+    int nn_inputs;
+    int nn_outputs;
+};
+
+}//are
+
+#endif //CPPNINDIVIDUAL_H
