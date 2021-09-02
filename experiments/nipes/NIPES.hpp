c869bc9204171d09a8b161d49f771f3df94e0025
MoxAntia <lenilegoff@gmail.com>
2021-09-02 17:25:08 +0100

    add obstacle avoidance task in nipes

--- experiments/nipes/NIPES.hpp
+++ experiments/nipes/NIPES.hpp
@@ -13,9 +13,36 @@
 #include "ARE/learning/Novelty.hpp"
 #include "simulatedER/nn2/NN2Individual.hpp"
 #include "ARE/Settings.h"
+#include "obstacleAvoidance.hpp"
 
 namespace are{
 
+
+typedef enum DescriptorType{
+    FINAL_POSITION = 0,
+    VISITED_ZONES = 1
+}DescriptorType;
+
+class NIPESIndividual : public sim::NN2Individual
+{
+public:
+    NIPESIndividual() : sim::NN2Individual(){}
+    NIPESIndividual(const Genome::Ptr& morph_gen,const NNParamGenome::Ptr& ctrl_gen)
+        : sim::NN2Individual(morph_gen,ctrl_gen){}
+    NIPESIndividual(const NIPESIndividual& ind)
+        : sim::NN2Individual(ind),
+          visited_zones(ind.visited_zones),
+          descriptor_type(ind.descriptor_type){}
+
+
+    Eigen::VectorXd descriptor() override;
+    void set_visited_zones(const Eigen::MatrixXi& vz){visited_zones = vz;}
+    void set_descriptor_type(DescriptorType dt){descriptor_type = dt;}
+private:
+
+    Eigen::MatrixXi visited_zones;
+    DescriptorType descriptor_type = FINAL_POSITION;
+};
 
 class NIPES : public EA
 {
