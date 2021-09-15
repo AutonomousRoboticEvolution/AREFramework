2ca584b8a355315306c086efd429edbd0f1dedbb
MoxAntia <lenilegoff@gmail.com>
2021-09-15 14:40:29 +0100

    update are_sim to work from any directory

--- are_sim.sh
+++ are_sim.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+directory=$(dirname $(readlink -f $0))
+
+
 if [ $1 == 'generate' ]
 then
-	mv libsimExtER.so libsimExtER.old
-	mv libsimExtGenerate.old libsimExtGenerate.so
-	rm libsimExtGenerate.old
+	mv -f $directory/libsimExtER.so $directory/libsimExtER.old
+	mv -f $directory/libsimExtGenerate.old $directory/libsimExtGenerate.so
+	rm -f $directory/libsimExtGenerate.old
 elif [ $1 == 'simulation' ]
 then
-	mv libsimExtGenerate.so libsimExtGenerate.old
-	mv libsimExtER.old libsimExtER.so
-	rm libsimExtER.old
+	mv -f $directory/libsimExtGenerate.so $directory/libsimExtGenerate.old
+	mv -f $directory/libsimExtER.old $directory/libsimExtER.so
+	rm -f $directory/libsimExtER.old
 else
 	echo "Error: first argument as to be generate or simulation"
 	exit 1
 fi
 
-./coppeliaSim.sh "${@#* }"
+
+$directory/coppeliaSim.sh "${@#* }"
