a84297a0f2ebc213cbb4d58a60a1bcbffd772413
matt <matt.hale@brl.ac.uk>
2021-09-16 15:39:18 +0100

    made timestep a paramter

--- PhysicalER/include/physicalER/pi/are_control.hpp
+++ PhysicalER/include/physicalER/pi/are_control.hpp
@@ -31,7 +31,7 @@ public:
 private:
     phy::NN2Individual controller;
     float _max_eval_time ; // millieconds
-    float _time_step = 1000; // milliseconds
+    float _time_step ; // milliseconds
 
     std::vector<Organ*> listOfOrgans;
 
