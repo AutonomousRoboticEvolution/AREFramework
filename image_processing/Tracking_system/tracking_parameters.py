763095b4fa333e5d8df0a1d383d52ddf767ff187
Matt <matt.hale@brl.ac.uk>
2022-02-14 18:00:14 +0000

    Tracking system changes initial commit

---
+++ image_processing/Tracking_system/tracking_parameters.py
@@ -0,0 +1,39 @@
+#tracking parameters
+import cv2
+from cv2 import aruco
+#blob detection parameters
+blob_detection_parameters = cv2.SimpleBlobDetector_Params()
+
+blob_detection_parameters.thresholdStep = 255
+blob_detection_parameters.minRepeatability = 1
+blob_detection_parameters.blobColor = 255
+blob_detection_parameters.filterByConvexity = False
+blob_detection_parameters.filterByArea = True
+blob_detection_parameters.maxArea = 5000000
+blob_detection_parameters.minArea = 100
+blob_detection_parameters.filterByInertia = True
+blob_detection_parameters.minInertiaRatio = 0.01
+
+# aruco dictionary and parameters
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+aruco_parameters = aruco.DetectorParameters_create()
+
+zmq_port_number= "5557"
+
+location = "bristol"
+
+#location specific parameters
+if location == "bristol":
+    # the pipe for getting images from the camera
+    pipe = "http://192.168.2.248/img/video.mjpeg"
+
+    #mask filter parameters
+    brainMin = (0,157,78)
+    brainMax = (26,255,224)
+
+    #centre of uncropped arena and ratio of pixels/metre
+    centre_reference = (386,264) # defined in the uncropped image
+    pixel_scale = 175
+
+    #crop parameters
+    crop_rectangle = [200,75,380,380]
