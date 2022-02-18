f3aa7336bfe73220c4eedcb1fd61dfacbbd98e74
ebb505@york.ac.uk <ebb505@york.ac.uk>
2022-02-18 11:48:48 +0000

    Gripper is now test against the skeleton. The orientation of the gripper seems off...

---
+++ experiments/scripts/edgar_dev/miscGeneralMethods.py
@@ -0,0 +1,46 @@
+import csv
+import numpy as np
+import os
+
+def getDirectories(repository):
+    print("Getting directories...")
+    # Get all directories
+    FOLDERS = []
+    FOLDERS_NAMES = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(repository):
+        for folder in d:
+            temp_folder = folder.split("_")
+            FOLDERS_NAMES.append(temp_folder[1])
+            FOLDERS.append(os.path.join(r, folder))
+    print("--- Folders: ", len(FOLDERS))
+    return FOLDERS, FOLDERS_NAMES
+
+def getDataFromFiles(folders, filename, startingColumn):
+    print("Getting data from files...")
+    dataFiles = []
+    for f in folders:
+        temp_filepath = f + '/' + filename
+        temp_list = []
+        if os.path.isfile(temp_filepath):
+            #print ("File exist: " + temp_filepath)
+            with open(temp_filepath, 'r') as csvfile:
+                data = csv.reader(csvfile, delimiter=',', quotechar='"')
+                for row in data:
+                    temp_list.append(row[startingColumn:-1]) # Last character is empty for some reason
+            dataFiles.append(temp_list)
+        else:
+            print ("File not exist: " + temp_filepath)
+            dataFiles.append(temp_list)
+    return dataFiles
+
+def transformDataToArray(data, numDirectories):
+    print("Transform data to array...")
+    # Transform to array
+    replicates_data = []
+    for rep in range(numDirectories):
+        tempDat = []
+        tempDat = np.array([[float(y) for y in x] for x in data[rep]]) # Convert to array
+        replicates_data.append(tempDat)
+    return replicates_data
+
