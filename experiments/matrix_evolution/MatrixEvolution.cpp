f9eb33fdd4ca86c892b1688c7f738bfc31147cf2
ebb505@york.ac.uk <ebb505@york.ac.uk>
2022-03-02 12:27:10 +0000

    It's now possible to select the distribution type

--- experiments/matrix_evolution/MatrixEvolution.cpp
+++ experiments/matrix_evolution/MatrixEvolution.cpp
@@ -219,33 +219,39 @@ std::vector<std::vector<double>> MATRIXEVOLUTION::mutate_matrix(std::vector<std:
     std::vector<std::vector<double>> mutated_matrix_4d = matrix_4d;
     float mutation_rate = settings::getParameter<settings::Float>(parameters,"#mutationRate").value;
     float mutation_parameter = settings::getParameter<settings::Float>(parameters,"#mutationParameter").value;
+    int mutation_type = settings::getParameter<settings::Integer>(parameters,"#mutationType").value;
     for(int i = 0; i < matrix_4d.size(); i++){
         for(int j = 0; j < matrix_4d.at(0).size(); j++){
             if(std::uniform_real_distribution<>(0,1)(nn2::rgen_t::gen) < mutation_rate) {
-                // Uniform distribution
-                double temp_variable;
-                double rand_number = std::uniform_real_distribution<>(-mutation_parameter,mutation_parameter)(nn2::rgen_t::gen);
-                temp_variable = rand_number  + matrix_4d.at(i).at(j);
-                if (temp_variable < -1.0)
-                    temp_variable = -1.0;
-                else if (temp_variable > 1.0)
-                    temp_variable = 1.0;
-                mutated_matrix_4d.at(i).at(j) = temp_variable;
-                // Polynomial mutation
-//                const float eta_m = mutation_parameter;
-//                assert(eta_m != -1.0f);
-//                float ri = std::uniform_real_distribution<>(0,1)(nn2::rgen_t::gen);
-//                float delta_i = ri < 0.5 ?
-//                                pow(2.0 * ri, 1.0 / (eta_m + 1.0)) - 1.0 :
-//                                1 - pow(2.0 * (1.0 - ri), 1.0 / (eta_m + 1.0));
-//                assert(!std::isnan(delta_i));
-//                assert(!std::isinf(delta_i));
-//                float temp_variable = matrix_4d.at(i).at(j) + delta_i;
-//                if (temp_variable < -1.0)
-//                    temp_variable = -1.0;
-//                else if (temp_variable > 1.0)
-//                    temp_variable = 1.0;
-//                mutated_matrix_4d.at(i).at(j) = temp_variable;
+                if(mutation_type == 0){ // Uniform distribution
+                    double temp_variable;
+                    double rand_number = std::uniform_real_distribution<>(-mutation_parameter,mutation_parameter)(nn2::rgen_t::gen);
+                    temp_variable = rand_number  + matrix_4d.at(i).at(j);
+                    if (temp_variable < -1.0)
+                        temp_variable = -1.0;
+                    else if (temp_variable > 1.0)
+                        temp_variable = 1.0;
+                    mutated_matrix_4d.at(i).at(j) = temp_variable;
+                }
+                else if(mutation_type == 1){ // Polynomial mutation
+                    const float eta_m = mutation_parameter;
+                    assert(eta_m != -1.0f);
+                    float ri = std::uniform_real_distribution<>(0,1)(nn2::rgen_t::gen);
+                    float delta_i = ri < 0.5 ?
+                                    pow(2.0 * ri, 1.0 / (eta_m + 1.0)) - 1.0 :
+                                    1 - pow(2.0 * (1.0 - ri), 1.0 / (eta_m + 1.0));
+                    assert(!std::isnan(delta_i));
+                    assert(!std::isinf(delta_i));
+                    float temp_variable = matrix_4d.at(i).at(j) + delta_i;
+                    if (temp_variable < -1.0)
+                        temp_variable = -1.0;
+                    else if (temp_variable > 1.0)
+                        temp_variable = 1.0;
+                    mutated_matrix_4d.at(i).at(j) = temp_variable;
+                } else{
+                    std::cerr << "Wrong mutation_type: " << mutation_type << " in " << __func__ << std::endl;
+                    assert(false);
+                }
             }
         }
     }
