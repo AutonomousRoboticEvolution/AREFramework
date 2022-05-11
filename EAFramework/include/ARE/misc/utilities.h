4540b1902bb121509ef0fcdef9fc14a2e0adcd30
Matt Hale <matt.hale@brl.ac.uk>
2022-05-11 14:21:42 +0100

    added comment for get_next_joint_position

--- EAFramework/include/ARE/misc/utilities.h
+++ EAFramework/include/ARE/misc/utilities.h
@@ -19,6 +19,14 @@ void split_line(const std::string &line,const std::string &delim,
 void stdvect_to_eigenvect(const std::vector<double>& std_v, Eigen::VectorXd &eigen_v);
 void eigenvect_to_stdvect(const Eigen::VectorXd &eigen_v, std::vector<double>& std_v);
 double sinusoidal(double amplitude, double time, double frequency, double time_offset, double position_offset);
+
+/**
+ * @brief get_next_joint_position - compute the new target angle based on the neural network and the current target value
+ * @param nn_output - in range [-1,1]
+ * @param time - in seconds
+ * @param previous_position - in radians
+ * @return the new target position, in radians
+ */
 double get_next_joint_position(double nn_output, double time, double previous_position);
 
 }//misc
