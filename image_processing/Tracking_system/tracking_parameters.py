463e9b6ec659c0e4a8273473e9f9fd505c7ca99e
yorkRoboFab <matt.hale@brl.ac.uk>
2022-05-19 14:22:59 +0100

    update colour definitions for York tracking system

--- image_processing/Tracking_system/tracking_parameters.py
+++ image_processing/Tracking_system/tracking_parameters.py
@@ -47,8 +47,8 @@ if location == "bristol":
 elif location == "york":
     pipe = 0
 
-    brainMin = (0,86,142)
-    brainMax = (179,255,255)
+    brainMin = (146,89,30)
+    brainMax = (179,184,110)
     
     centre_reference = (340,300) #not calibrated
     pixel_scale = 100 #nc
