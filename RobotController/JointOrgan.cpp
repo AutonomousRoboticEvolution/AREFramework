1be9ce42247f343152a27cee89645ab71319510f
Matt Hale <matt.hale@brl.ac.uk>
2022-05-25 14:00:46 +0100

    tidy up debug prints

--- RobotController/JointOrgan.cpp
+++ RobotController/JointOrgan.cpp
@@ -18,9 +18,8 @@ JointOrgan::JointOrgan(uint8_t address) : Organ(address){
 
 //new Target is a signed value in degrees for the target angle to move to
 void JointOrgan::setTargetAngle(int8_t newTarget) {
+    if(DEBUG_PRINT){std::cout<<"Setting target angle to: "<<newTarget<<std::endl;}
 	//Send to device (write8To inherited from I2CDevice)
-    std::clog << static_cast<int>(newTarget);
-
     write8To(TARGET_POSITION_REGISTER, newTarget);
 }
 
@@ -33,16 +32,13 @@ void JointOrgan::setLEDBrightness(int8_t newValue) {
 
 //new Target is a signed float between -1 and 1, which will be converted to angle of +/-MAX_MAGNITUDE_TARGET_ANGLE degrees
 void JointOrgan::setTargetAngleNormalised(float newTargetNormalised) {
-	if (newTargetNormalised>1){
-        if(DEBUG_PRINT){std::cout<<"Setting target angle to: "<<MAX_MAGNITUDE_TARGET_ANGLE<<std::endl;}
+    if (newTargetNormalised>1){
         setTargetAngle(MAX_MAGNITUDE_TARGET_ANGLE);
     }
-	else if (newTargetNormalised<-1){
-        if(DEBUG_PRINT){std::cout<<"Setting target angle to: "<<-MAX_MAGNITUDE_TARGET_ANGLE<<std::endl;}
+    else if (newTargetNormalised<-1){
         setTargetAngle(-MAX_MAGNITUDE_TARGET_ANGLE);
     }
     else{
-        if(DEBUG_PRINT){std::cout<<"Setting target angle to: "<<(MAX_MAGNITUDE_TARGET_ANGLE*newTargetNormalised)<<std::endl;}
         setTargetAngle(MAX_MAGNITUDE_TARGET_ANGLE*newTargetNormalised);
     }
 }
