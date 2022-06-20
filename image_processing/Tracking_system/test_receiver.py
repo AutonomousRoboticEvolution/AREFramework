53a7f0cf3df4c25a9145020e5866408278f1f075
Matt <matt.hale@brl.ac.uk>
2022-06-20 17:05:15 +0100

    updated test script for tracking

--- image_processing/Tracking_system/test_receiver.py
+++ image_processing/Tracking_system/test_receiver.py
@@ -9,7 +9,7 @@ PRINT_INFO = False
 #  Prepare our context and sockets
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5557")
+socket.connect("tcp://192.168.2.247:5557")
 
 socket.send_string("Recording:start")
 assert( socket.recv_string() == "OK")
@@ -30,26 +30,33 @@ print("Resolution is {} x {} , {}".format(width,height, depth))
 
 start_time = time.time()
 
-while time.time() < start_time + 10:
-
-    # get image:
-    if PRINT_INFO: print("Requesting image...")
-    socket.send_string("Image:image")
-    message = socket.recv()
-    if PRINT_INFO: print("Got message type: {}".format(type(message)))
-    image = numpy.frombuffer(message, dtype=numpy.uint8)
-    if PRINT_INFO: print(image[0])
-    if PRINT_INFO: print("image is type: {}".format(type(image)))
-    if PRINT_INFO: print(width*height*depth)
-    image = image.reshape(width,height,depth)
-    if PRINT_INFO: print("image shape is {}".format(image.shape))
+while time.time() < start_time + 5:
 
     if SHOW_IMAGES:
+        # get image:
+        if PRINT_INFO: print("Requesting image...")
+        socket.send_string("Image:image")
+        message = socket.recv()
+        if PRINT_INFO: print("Got message type: {}".format(type(message)))
+        image = numpy.frombuffer(message, dtype=numpy.uint8)
+        if PRINT_INFO: print(image[0])
+        if PRINT_INFO: print("image is type: {}".format(type(image)))
+        if PRINT_INFO: print(width*height*depth)
+        image = image.reshape(width,height,depth)
+        if PRINT_INFO: print("image shape is {}".format(image.shape))
+
         cv2.imshow("receiver", image)
         cv2.waitKey(1)
+
+    else:
+        socket.send_string("Robot:position")
+        message = socket.recv()
+        print(message)
+
     time.sleep(0.1)
 
-print("saving")
-socket.send_string("Recording:save_test_video2")
+filename = str( int( time.time() ) )
+print("saving {}".format(filename))
+socket.send_string("Recording:save_{}".format(filename))
 assert( socket.recv_string() == "OK")
 print("finished")
