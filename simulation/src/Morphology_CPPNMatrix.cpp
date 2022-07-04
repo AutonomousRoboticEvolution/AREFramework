3b0fcb02c8593bc80876a63f9aedcd2c1109d87e
ebb505@york.ac.uk <ebb505@york.ac.uk>
2022-07-04 16:13:18 +0100

    Camera fixed

--- simulation/src/Morphology_CPPNMatrix.cpp
+++ simulation/src/Morphology_CPPNMatrix.cpp
@@ -319,6 +319,7 @@ void Morphology_CPPNMatrix::create()
             }
         }
     }
+//    usleep(1000000);
     if(settings::getParameter<settings::Boolean>(parameters,"#isCPPNGenome").value)
         retrieve_matrices_from_cppn();
     // Get info from body plan for body plan descriptors or logging.
@@ -583,47 +584,47 @@ void Morphology_CPPNMatrix::exportRobotModel(int indNum)
 
     // Export blueprint
 
-//    std::stringstream sst_blueprint;
-//    sst_blueprint << "/blueprint_" << "0" << ".csv";
-//    std::ofstream ofs_blueprint(Logging::log_folder + sst_blueprint.str());
-//    if(!ofs_blueprint)
-//    {
-//        std::cerr << "unable to open : " << Logging::log_folder  + sst_blueprint.str() << std::endl;
-//        return;
-//    }
-//    std::vector<int> tempOrganTypes = blueprint.getOrganTypes();
-//    std::vector<std::vector<float>> tempOrganPos = blueprint.getOrganPosList();
-//    std::vector<std::vector<float>> tempOrganOri = blueprint.getOrganOriList();
-//    for (int i = 0; i < tempOrganTypes.size(); i++) {
-//        ofs_blueprint << "0" << "," << tempOrganTypes.at(i) << ","
-//                      << tempOrganPos.at(i).at(0) << "," << tempOrganPos.at(i).at(1) << ","
-//                      << tempOrganPos.at(i).at(2) << ","
-//                      << tempOrganOri.at(i).at(0) << "," << tempOrganOri.at(i).at(1) << ","
-//                      << tempOrganOri.at(i).at(2) << ","
-//                      << std::endl;
-//    }
-//
-//    // Export mesh file
-//    const auto **verticesMesh = new const simFloat *[2];
-//    const auto **indicesMesh = new const simInt *[2];
-//    auto *verticesSizesMesh = new simInt[2];
-//    auto *indicesSizesMesh = new simInt[2];
-//    verticesMesh[0] = skeletonListVertices.data();
-//    verticesSizesMesh[0] = skeletonListVertices.size();
-//    indicesMesh[0] = skeletonListIndices.data();
-//    indicesSizesMesh[0] = skeletonListIndices.size();
-//
-//    std::stringstream filepath_mesh;
-//    filepath_mesh << Logging::log_folder << "/mesh_" << "0" << ".stl";
-//
-//    //fileformat: the fileformat to export to:
-//    //  0: OBJ format, 3: TEXT STL format, 4: BINARY STL format, 5: COLLADA format, 6: TEXT PLY format, 7: BINARY PLY format
-//    simExportMesh(3, filepath_mesh.str().c_str(), 0, 1.0f, 1, verticesMesh, verticesSizesMesh, indicesMesh, indicesSizesMesh, nullptr, nullptr);
-//
-//    delete[] verticesMesh;
-//    delete[] verticesSizesMesh;
-//    delete[] indicesMesh;
-//    delete[] indicesSizesMesh;
+    std::stringstream sst_blueprint;
+    sst_blueprint << "/blueprint_" << "0" << ".csv";
+    std::ofstream ofs_blueprint(Logging::log_folder + sst_blueprint.str());
+    if(!ofs_blueprint)
+    {
+        std::cerr << "unable to open : " << Logging::log_folder  + sst_blueprint.str() << std::endl;
+        return;
+    }
+    std::vector<int> tempOrganTypes = blueprint.getOrganTypes();
+    std::vector<std::vector<float>> tempOrganPos = blueprint.getOrganPosList();
+    std::vector<std::vector<float>> tempOrganOri = blueprint.getOrganOriList();
+    for (int i = 0; i < tempOrganTypes.size(); i++) {
+        ofs_blueprint << "0" << "," << tempOrganTypes.at(i) << ","
+                      << tempOrganPos.at(i).at(0) << "," << tempOrganPos.at(i).at(1) << ","
+                      << tempOrganPos.at(i).at(2) << ","
+                      << tempOrganOri.at(i).at(0) << "," << tempOrganOri.at(i).at(1) << ","
+                      << tempOrganOri.at(i).at(2) << ","
+                      << std::endl;
+    }
+
+    // Export mesh file
+    const auto **verticesMesh = new const simFloat *[2];
+    const auto **indicesMesh = new const simInt *[2];
+    auto *verticesSizesMesh = new simInt[2];
+    auto *indicesSizesMesh = new simInt[2];
+    verticesMesh[0] = skeletonListVertices.data();
+    verticesSizesMesh[0] = skeletonListVertices.size();
+    indicesMesh[0] = skeletonListIndices.data();
+    indicesSizesMesh[0] = skeletonListIndices.size();
+
+    std::stringstream filepath_mesh;
+    filepath_mesh << Logging::log_folder << "/mesh_" << "0" << ".stl";
+
+    //fileformat: the fileformat to export to:
+    //  0: OBJ format, 3: TEXT STL format, 4: BINARY STL format, 5: COLLADA format, 6: TEXT PLY format, 7: BINARY PLY format
+    simExportMesh(3, filepath_mesh.str().c_str(), 0, 1.0f, 1, verticesMesh, verticesSizesMesh, indicesMesh, indicesSizesMesh, nullptr, nullptr);
+
+    delete[] verticesMesh;
+    delete[] verticesSizesMesh;
+    delete[] indicesMesh;
+    delete[] indicesSizesMesh;
 }
 
 void Morphology_CPPNMatrix::testRobot(PolyVox::RawVolume<uint8_t>& skeletonMatrix)
