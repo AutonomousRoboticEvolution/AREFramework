e0c141ea2e17c4a2e88df9e57980054913d01961
ebb505@york.ac.uk <ebb505@york.ac.uk>
2022-08-15 16:35:03 +0100

    Scripts updated

--- experiments/scripts/edgar_dev/bash_scripts/experiments/download_specific_files.sh
+++ experiments/scripts/edgar_dev/bash_scripts/experiments/download_specific_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ISVIKING=0
-ISAREPC=1
+ISVIKING=1
+ISAREPC=0
 if [[ $ISVIKING == 1 ]]; then
 	echo "Downloading from viking"
 	ORIGIN="ebb505@viking.york.ac.uk:/users/ebb505/scratch/are-logs/mnipes_experiments/exploration/4/mnipes*"
@@ -12,7 +12,7 @@ else
 		echo "Wrong!"
 	fi
 fi
-DESTINATION="/home/edgar/are-logs/mnipes_experiments/exploration/4/"
+DESTINATION="/media/ebb505/ARE-EBB/ARE/are-logs/mnipes_experiments/exploration/4_10/"
 #FILES=('morph_descriptors.csv')
 FILES=('parameters.csv' 'fitnesses.csv' 'morph_descriptors.csv' 'traj*')
 #FILES=('parameters.csv' 'fitnesses.csv' 'MD_Cart_WDH.csv' 'parenting.csv' 'organ_pos_desc.csv' 'nbr_conn_neu.csv')
