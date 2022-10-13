d115d0235846308aaae682afb538f86fa12d6793
MoxAntia <lenilegoff@gmail.com>
2022-10-13 14:39:23 +0100

    add option to create separate ctrl_genome file from robot_infos file with read_robot_infos.py

--- PhysicalER/python/read_robot_infos.py
+++ PhysicalER/python/read_robot_infos.py
@@ -31,6 +31,33 @@ def load_robot_infos(filename):
 					trajectories[eval_nbr[-1]] = [[float(p) for p in line.split(";")[0].split(",")]]
 		return eval_nbr, fitnesses, trajectories
 
+def load_ctrl_genome(eval_nbr,filename):
+	with open(filename) as file:
+		lines = file.readlines()
+		ctrl_genome_lines = []
+		e = 0
+		state = 0
+		finished = False
+		for line in lines:
+			if(line.split("_")[0] == "evaluation" and len(ctrl_genome_lines) == 0):
+				e = int(line.split("_")[1])
+			elif(line.split("_")[0] == "evaluation" and len(ctrl_genome_lines) > 0):
+				finished = True
+			elif(line.split(";")[0] == "ctrl_genome" and e == eval_nbr):
+				state = 1
+			elif(state == 1):
+				ctrl_genome_lines.append(line)
+
+			if(finished):
+				break
+
+		return ctrl_genome_lines
+
+def write_ctrl_genome(robot_id,eval_nbr,ctrl_genome_lines):
+	filename = "ctrl_genome_" + str(robot_id) + "_" + str(eval_nbr)
+	with open(filename,'w') as file:
+		file.writelines(ctrl_genome_lines)
+
 def plot_fitnesses(eval_nbr,fitnesses):
 	lines = [[e,f] for e, f in zip(eval_nbr,fitnesses)]
 	data = pd.DataFrame(data=lines,columns=["eval","fitness"])
@@ -60,6 +87,7 @@ if __name__ == '__main__':
 	parser.add_argument('-f',help="plot fitnesses")
 	parser.add_argument('--traj',type=int,default=0,help="plot trajectory of specific evaluation")
 	parser.add_argument('--best',type=int,default=1,help="print N best fitnesses and corresponding eval number")
+	parser.add_argument('--ctrl',type=int,default=0,help="write ctrl genome from evaluation number ctrl")
 	args = parser.parse_args()
 	
 	evals, fitnesses, trajectories = load_robot_infos(args.filename)
@@ -67,6 +95,18 @@ if __name__ == '__main__':
 		print(best_fitnesses(args.best,evals,fitnesses))
 	if(args.f):
 		plot_fitnesses(evals,fitnesses)
-	if(not args.traj == 0):
-		plot_trajectory(args.traj,trajectories)
+	if(args.traj > 0):
+		if(args.traj >= len(evals)):
+			print("Error: there is no evaluation number",args.ctrl)
+		else:
+			plot_trajectory(args.traj,trajectories)
+	if(args.ctrl > 0):
+		if(args.ctrl >= len(evals)):
+			print("Error: there is no evaluation number",args.ctrl)
+		else:
+			ctrl_genome_lines = load_ctrl_genome(args.ctrl,args.filename)
+			robot_id = int(args.filename.split("/")[-1].split("_")[-1])
+			write_ctrl_genome(robot_id,args.ctrl,ctrl_genome_lines)
+		
+			
 			
