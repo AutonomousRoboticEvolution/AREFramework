3e192c3853dc186f82ef27f561053a302fd5bdc1
Simon Smith <artificialsimon@gmail.com>
2024-05-08 17:28:30 +0100

    Moved parameters inside folder for better organisation

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo './build.sh --build-arg="REBUILD_ARE=1" to force rebuild ARE'
-sudo docker build --build-arg="N_CPU=20" $1 -t artificialsimon/are_framework:melai .
+sudo docker build --build-arg="N_CPU=20" $1 -t artificialsimon/areframework:melai .
