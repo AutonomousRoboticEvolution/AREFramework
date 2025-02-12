32a48776c599e12808570f70fb25dfedbe1a8ef2
Leni Le Goff <leni_legoff@proton.me>
2025-02-12 11:59:29 +0000

    refactor superquadrics in a separate file from sq_cppn_genome.*

--- simulation/src/nn2/sq_cppn_genome.cpp
+++ simulation/src/nn2/sq_cppn_genome.cpp
@@ -8,67 +8,6 @@ using namespace are;
 using namespace are::sq_cppn;
 using namespace sim::organ;
 
-quadric_param_t::quadric_param_t(double _a, double _b, double _c,
-                                 double _u, double _v,
-                                 double _r, double _s, double _t):
-        a(_a), b(_b), c(_c), u(_u), v(_v), r(_r), s(_s), t(_t){
-}
-void quadric_param_t::random(const misc::RandNum::Ptr& rn){
-    a += rn->randDouble(-0.5,0.5);
-    if(a == 0)
-        a = 0.0001;
-    b += rn->randDouble(-0.5,0.5);
-    if(b == 0)
-        b = 0.0001;
-    c += rn->randDouble(-0.5,0.5);
-    if(c == 0)
-        c = 0.0001;
-    u += rn->randDouble(-0.5,0.5);
-    if(u == 0)
-        u = 0.0001;
-    v += rn->randDouble(-0.5,0.5);
-    if(v == 0)
-        v = 0.0001;
-    r += rn->randDouble(-0.5,0.5);
-    if(r == 0)
-        r = 0.0001;
-    s += rn->randDouble(-0.5,0.5);
-    if(s == 0)
-        s = 0.0001;
-    t += rn->randDouble(-0.5,0.5);
-    if(t == 0)
-        t = 0.0001;
-}
-
-std::string quadric_param_t::to_string() const{
-    std::stringstream sstr;
-    sstr << a << ","
-         << b << ","
-         << c << ","
-         << u << ","
-         << v << ","
-         << r << ","
-         << s << ","
-         << t;
-    return sstr.str();
-}
-
-void quadric_param_t::from_string(const std::string& str){
-    std::vector<std::string> splitted_str;
-    misc::split_line(str,",",splitted_str);
-    a = std::stod(splitted_str[0]);
-    b = std::stod(splitted_str[1]);
-    c = std::stod(splitted_str[2]);
-    u = std::stod(splitted_str[3]);
-    v = std::stod(splitted_str[4]);
-    r = std::stod(splitted_str[5]);
-    s = std::stod(splitted_str[6]);
-    t = std::stod(splitted_str[7]);
-}
-
-double quadric_params::_mutation_rate = 0.1f;
-double quadric_params::_sigma = 0.1f;
-
 int sq_cppn::params::cppn::_mutation_type = 0; //uniform
 bool sq_cppn::params::cppn::_mutate_connections = true;
 bool sq_cppn::params::cppn::_mutate_neurons = true;
