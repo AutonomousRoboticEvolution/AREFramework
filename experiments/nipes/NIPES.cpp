819790f6ac2670826c10b2abb0d413211ce174db
Leni Le Goff <leni_legoff@proton.me>
2025-10-21 17:08:24 +0100

    add CPGRBF network type to NIPES

--- experiments/nipes/NIPES.cpp
+++ experiments/nipes/NIPES.cpp
@@ -10,6 +10,7 @@ void NIPESIndividual::createController(){
 
     int nn_type = settings::getParameter<settings::Integer>(parameters,"#NNType").value;
     int nb_hidden = settings::getParameter<settings::Integer>(parameters,"#NbrHiddenNeurones").value;
+    int nb_rbf = settings::getParameter<settings::Integer>(parameters,"#NbrRBFNeurones").value;
     const std::vector<int> joint_subs = settings::getParameter<settings::Sequence<int>>(parameters,"#jointSubs").value;
 
     int wheel_nbr,joint_nbr,sensor_nbr;
@@ -41,12 +42,25 @@ void NIPESIndividual::createController(){
         std::cout << "Creating MLP with " << nb_inputs << " inputs, " << nb_hidden << " hidden neurons, and " << nb_outputs << " ouputs." << std::endl;
         control = std::make_shared<MLPControl>(nb_inputs,nb_outputs,nb_hidden);
         std::dynamic_pointer_cast<MLPControl>(control)->_nn->set_weights_biases(weights,bias);
-        // std::vector<double> out_weights =  std::dynamic_pointer_cast<MLPControl>(control)->_nn->get_weights();
     }else if(nn_type == tnn::t_RNN){
         std::cout << "Creating RNN with " << nb_inputs << " inputs, " << nb_hidden << " hidden neurons, and " << nb_outputs << " ouputs." << std::endl;
         control = std::make_shared<RNNControl>(nb_inputs,nb_outputs,nb_hidden);
         std::dynamic_pointer_cast<RNNControl>(control)->_nn->set_weights_biases(weights,bias);
-        // std::vector<double> out_weights =  std::dynamic_pointer_cast<MLPControl>(control)->_nn->get_weights();
+    }else if(nn_type == tnn::t_CPGRBF){
+        std::cout << "Creating CPGRBFNetwork with " << nb_rbf <<" RBF neurons, and " << nb_outputs << " ouputs." << std::endl;
+        control = std::make_shared<CPGRBFControl>(nb_outputs,nb_rbf);
+        std::dynamic_pointer_cast<CPGRBFControl>(control)->_nn->set_out_layer_parameters(weights,bias);
+    }else if(nn_type == tnn::t_CPGRBFRNN){
+        std::cout << "Creating CPGRBFRNN with " << nb_inputs << " inputs, " << nb_hidden << " recurrent neurons, " << nb_rbf <<" RBF neurons, and " << nb_outputs << " ouputs." << std::endl;
+        control = std::make_shared<CPGRBFRNNControl>(nb_inputs,nb_outputs,nb_rbf, nb_hidden);
+        std::vector<double> out_ws(weights.begin(),weights.begin() + nb_rbf*nb_outputs);
+        std::vector<double> rnn_ws(weights.begin()+nb_rbf*nb_outputs,weights.end());
+        std::vector<double> out_bs(bias.begin(),bias.begin()+nb_outputs);
+        std::vector<double> rnn_bs(bias.begin()+nb_outputs,bias.end());
+        std::cout << out_ws.size() << "," << rnn_ws.size() << std::endl;
+        std::dynamic_pointer_cast<CPGRBFRNNControl>(control)->_nn->set_out_layer_parameters(out_ws,out_bs);
+        std::dynamic_pointer_cast<CPGRBFRNNControl>(control)->_nn->set_rnn_parameters(rnn_ws,rnn_bs);
+        
     }
     else {
         std::cerr << "unknown type of neural network" << std::endl;
@@ -146,6 +160,7 @@ void NIPES::init(){
         int nn_type = settings::getParameter<settings::Integer>(parameters,"#NNType").value;
         const int nb_input = settings::getParameter<settings::Integer>(parameters,"#NbrInputNeurones").value;
         const int nb_hidden = settings::getParameter<settings::Integer>(parameters,"#NbrHiddenNeurones").value;
+        const int nb_rbf = settings::getParameter<settings::Integer>(parameters,"#NbrRBFNeurones").value;
         const int nb_output = settings::getParameter<settings::Integer>(parameters,"#NbrOutputNeurones").value;
         const std::vector<int> joint_subs = settings::getParameter<settings::Sequence<int>>(parameters,"#jointSubs").value;
 
@@ -170,6 +185,14 @@ void NIPES::init(){
             MLPControl::nbr_parameters(nb_input,nb_output,nb_hidden,nbr_weights,nbr_bias);
         else if(nn_type == tnn::t_RNN)
             RNNControl::nbr_parameters(nb_input,nb_output,nb_hidden,nbr_weights,nbr_bias);
+        else if(nn_type == tnn::t_CPGRBF)
+            CPGRBFControl::nbr_parameters(nb_output,nb_rbf,nbr_weights,nbr_bias);
+        else if(nn_type == tnn::t_CPGRBFRNN){
+            int nbr_out_weights,nbr_rnn_weights,nbr_rnn_biases,nbr_out_biases;
+            CPGRBFRNNControl::nbr_parameters(nb_input,nb_output,nb_rbf,nb_hidden,nbr_rnn_weights,nbr_out_weights,nbr_rnn_biases,nbr_out_biases);
+            nbr_weights = nbr_rnn_weights + nbr_out_weights;
+            nbr_bias = nbr_rnn_weights + nbr_out_biases;
+        }
         else {
             std::cerr << "unknown type of neural network" << std::endl;
             return;
